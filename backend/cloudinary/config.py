7493a4084cd564fecd7bce6214028fa8ff8a5819
HARISHH A <harishh.ad21@bitsathy.ac.in>
2025-02-21 16:30:08 +0000

    Update backend/cloudinary/config.py

--- backend/cloudinary/config.py
+++ backend/cloudinary/config.py
@@ -4,7 +4,7 @@ import cloudinary.api
 
 # Cloudinary Configuration
 cloudinary.config(
-    cloud_name="dhenxgofs",  # Replace with your Cloud name
-    api_key="316433455823366",        # Replace with your API Key
-    api_secret= "fQ441adO1OI_Rr8zhGpOFHTodAI" 
+    cloud_name="===============",  # Replace with your Cloud name
+    api_key="===============",        # Replace with your API Key
+    api_secret= "=================" 
 )
